3884eb0b8e73290ab00ab6cc053e7748c2cbbcf3
Hong Jiang <hong.jiang@onycloud.com>
2011-08-23 14:47:41 +0800

    Add JSON-related middleware.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject onycloud-middleware "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.mongodb/bson "2.5.3"]])
---
+++ src/onycloud/middleware/json.clj
@@ -0,0 +1,38 @@
+(ns onycloud.middleware.json
+  (:use [clojure.walk :only [prewalk]])
+  (:require [clojure.data.json :as json])
+  (:import org.bson.types.ObjectId))
+
+(defn- remove-mongo-id [form]
+  (if (and (map? form) (= ObjectId (class (:_id form))))
+    (dissoc form :_id)
+    form))
+
+(defn json-response
+  "Construct a JSON HTTP response."
+  [status & [body]]
+  {:status status
+   :headers {"Content-Type" "application/json; charset=utf-8"}
+   :body (json/json-str (prewalk remove-mongo-id (or body "")))})
+
+(defn wrap-json
+  [handler]
+  (fn [req]
+    ;; it must be json, keywordize by default
+    (let [json-req-body (if-let [body (:body req)]
+                          (let [body-str (cond (string? body) body
+                                               :else (slurp body))]
+                            (when-not (empty? body-str)
+                              (json/read-json body-str))))
+          ;; TODO better name of :json-body
+          req* (assoc req
+                 :json-body json-req-body)
+          resp-obj (handler req*)
+          resp (let [status (:status resp-obj)]
+                 (if (number? status)
+                   (json-response status (:body resp-obj))
+                   (json-response 200 resp-obj)))]
+      resp)))
+
+(defmacro JSON [verb path args handler]
+  `(~verb ~path ~args (wrap-json ~handler)))
